4c9133539e9704757d36e69dea06cb2fe834a488
lssHaiXue <280508950@qq.com>
2018-04-17 14:23:23 +0800

    新建一个名为webpack.config.server.js的文件，用来作为webpack打包服务端代码的配置文件

---
+++ build/webpack.config.server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    target: 'node', //代表打包出来的内容的执行环境
+    entry: {
+        app: path.join(__dirname, '../client/server.entry.js')
+    },
+    output: {
+        filename: 'server.output.js',
+        path: path.join(__dirname, '../dist'),
+        publicPath: '',
+        libraryTarget: 'commonjs2' //使用模块的方案，AMD,CMD....
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: [
+                    path.join(__dirname, '../node_modules')
+                ]
+            }
+        ]
+    }
+}
