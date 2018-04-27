b21fbd9a8e69e571be252e4c0a7170158a054716
lssHaiXue <280508950@qq.com>
2018-04-27 11:49:19 +0800

    优化客户端与服务端的webpack.config.js

--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -1,37 +1,14 @@
 const path = require('path')
+const baseConfig = require('./webpack.base')
+const webpackMerge = require('webpack-merge')
 
-module.exports = {
+module.exports = webpackMerge(baseConfig, {
   target: 'node', // 代表打包出来的内容的执行环境
   entry: {
     app: path.join(__dirname, '../client/server.entry.js')
   },
   output: {
     filename: 'server.output.js',
-    path: path.join(__dirname, '../dist'),
-    publicPath: '/public',
     libraryTarget: 'commonjs2' // 使用模块的方案，AMD,CMD....
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /.(js|jsx)$/,
-        loader: 'eslint-loader',
-        exclude: [
-          path.resolve(__dirname, '../node_modules')
-        ]
-      },
-      {
-        test: /.jsx$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /.js$/,
-        loader: 'babel-loader',
-        exclude: [
-          path.join(__dirname, '../node_modules')
-        ]
-      }
-    ]
   }
-}
+})
